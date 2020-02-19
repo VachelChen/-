b9fb8656979f3ae30e1330dc05dc4f9a06bac734
vachel <814018236@qq.com>
2020-02-19 14:37:22 +0800

    地宫取宝算法更新，采用记录重复子问题来进行剪枝

--- 2014_地宫夺宝/2014_地宫夺宝/main.cpp
+++ 2014_地宫夺宝/2014_地宫夺宝/main.cpp
@@ -7,9 +7,56 @@
 //
 
 #include <iostream>
+#include <cstring>
+
+using namespace std;
+
+long long cache[50][50][14][13];
+int value[50][50];
+
+int n,m,k;
+const int MOD = 1000000007;
+
+long long dfs(int x,int y,int maxValue,int num){
+    
+    if(cache[x][y][maxValue+1][num] != -1)   {
+        return cache[x][y][maxValue+1][num];
+    }
+    
+    if (x == n || y == m || num > k)    return 0;
+    
+    long long ans=0;
+    
+    if(x == n-1 && y == m-1 )  {
+        if(num == k || (num == k-1 && maxValue<value[x][y])){
+            ans++;
+            if(ans>MOD)
+                ans=ans%MOD;
+        }
+        return ans;
+    }
+    
+    //cout<<maxValue<<" "<<value[x][y]<<endl;
+    if(maxValue<value[x][y]){
+        ans+=dfs(x+1, y, value[x][y], num+1);
+        ans+=dfs(x, y+1, value[x][y], num+1);
+    }
+    ans+=dfs(x+1, y, maxValue, num);
+    ans+=dfs(x, y+1, maxValue, num);
+    
+    cache[x][y][maxValue+1][num] = ans % MOD;
+    return cache[x][y][maxValue+1][num];
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    cin>>n>>m>>k;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin>>value[i][j];
+        }
+    }
+    memset(cache,-1,sizeof(cache));
+    cout<<dfs(0,0,-1,0)<<endl;
     return 0;
 }
