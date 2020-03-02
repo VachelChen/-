32580a5d0d306ece2e552d594f4271c6d7a94052
vachel <814018236@qq.com>
2020-03-02 22:53:27 +0800

    change

--- codeup_查找元素_统计同城学生人数/codeup_查找元素_统计同城学生人数/main.cpp
+++ codeup_查找元素_统计同城学生人数/codeup_查找元素_统计同城学生人数/main.cpp
@@ -9,35 +9,12 @@
 #include <iostream>
 using namespace std;
 
-struct student{
-    string num;
-    string name;
-    string sex;
-    int age;
-};
-
 int main(int argc, const char * argv[]) {
     int n;
     while(cin>>n){
-    student stu[1000];
-    for(int i=0;i<n;i++){
-        cin>>stu[i].num>>stu[i].name>>stu[i].sex>>stu[i].age;
-    }
-    int m;
-    cin >>m;
-    for(int i=0;i<m;i++){
-        string number;
-        cin>>number;
-        int j;
-        for(j=0;j<n;j++){
-            if(stu[j].num == number){
-                cout<<stu[j].num<<" "<<stu[j].name<<" "<<stu[j].sex<<" "<<stu[j].age<<endl;;
-                break;
-            }
-        }
-        if(j==n)
-            cout<<"No Answer!"<<endl;
-    }
+        int a[100];
+        for(int i=0;i<n;i++)
+            cin>>a[]
     }
     return 0;
 }
