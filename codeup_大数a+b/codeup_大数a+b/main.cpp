64728452c6c6ca573809f22c9dc450435773e829
vachel <814018236@qq.com>
2020-03-11 00:20:34 +0800

    c

--- codeup_大数a+b/codeup_大数a+b/main.cpp
+++ codeup_大数a+b/codeup_大数a+b/main.cpp
@@ -7,81 +7,71 @@
 //
 
 #include <iostream>
-#include <algorithm>
-#include <sstream>
+#include <cstring>
 using namespace std;
 
-string i2s(int x){
-    string s;
-    stringstream ss;
-    ss << x;
-    ss >> s;
-    return s;
+struct bign{
+  int d[1000];
+  int len;
+  //因为结构体中有数组，定义的时候需要初始化一下。
+  bign(){
+    memset(d,0,sizeof(d));
+    len = 0;
+  }
+};
+
+bign change(string str){
+  bign a;
+  a.len = str.size();
+  for(int i=0;i<a.len;i++)
+    a.d[i] = str[a.len-i-1] - '0';
+  return a;
 }
 
-int s2i(char s){
-    int x;
-    stringstream ss;
-    ss << s;
-    ss >> x;
-    return x;
+
+bign add(bign a,bign b){
+  bign c;
+  int carry = 0;
+  for(int i=0;i<max(a.len,b.len);i++){
+    int temp = a.d[i] + b.d[i] + carry;
+    c.d[c.len++] = temp%10;
+    carry = temp/10;
+  }
+  if(carry!=0){
+    c.d[c.len++] = carry;
+  }
+  return c;
 }
 
-int fushu(string a){
-    if(a[0] == '-')
-        return 1;
-    else
-        return 0;
-}
+//必须要a>b
+bign sub(bign a,bign b){
+  bign c;
+  for(int i=0;i<max(a.len,b.len);i++){
+    if(a.d[i]<b.d[i]){
+      a.d[i+1]--;
+      a.d[i] += 10;
+    }
+    c.d[c.len++] = a.d[i] - b.d[i];
+  }
+  while(c.len >=2 && c.d[c.len-1] == 0)
+  {c.len--;}
 
-bool bBigger(string a,string b){
-    if(b.size()>a.size()) return true;
-    if(b)
+  return c;
 }
 
 int main(int argc, const char * argv[]) {
-    string a,b;
+    string x,y;
     string ans;
-    while (cin >> a >> b) {
-        if(fushu(a)+fushu(b)!=1){
-            reverse(a.begin(),a.end());
-            reverse(b.begin(),b.end());
-            int carry=0;
-            for(int i=0;i<max(a.size(),b.size());i++){
-                int temp = s2i(a[i]) + s2i(b[i]) + carry;
-                ans.append(i2s(temp % 10));
-                carry = temp/10;
-            }
-            if (carry != 0) {
-                ans.append(i2s(1));
-            }
+    while (cin >> x >> y) {
+        bign a,b,ans;
+        a = change(x);
+        b = change(y);
+        ans = sub(a, b);
+        for(int i=ans.len-1;i>=0;i--){
+            cout<<ans.d[i];
         }
-        else{
-            if(fushu(a)){
-                string temp = a;
-                a = b;
-                b = temp;
-            }
-            b = b.substr(1,sizeof(b));
-            reverse(a.begin(),a.end());
-            reverse(b.begin(),b.end());
-            bool ansSymbol = false;
-            if(a.size()<b.size())
-                ansSymbol = true;
-            int borrow = 0;
-            for (int i=0; i<a.size(); i++) {
-                int temp = a[i] - b[i] - borrow;
-                ans.append(temp>=0?i2s(temp):i2s(abs(temp)));
-                if(temp<0)  borrow = 1;
-                else    borrow = 0;
-            }
-            if(borrow == 1)
-                
-            
-        }
-        reverse(ans.begin(), ans.end());
-        cout<<ans<<endl;
-        a = b = ans = "";
+        
+        cout<<endl;
     }
     return 0;
 }
