d1953691d17839ff6d093d8a19e6f4654d8aec3a
vachel <814018236@qq.com>
2020-03-11 22:16:35 +0800

    快速球n！中的质因数p的个数

--- 基础测试/基础测试/main.cpp
+++ 基础测试/基础测试/main.cpp
@@ -12,57 +12,19 @@
 #include <queue>
 using namespace std;
 
-struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     //TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-bool isSymmetric(TreeNode* root) {
-    bool ans = true;
-        if(root == NULL)    return ans;
-        if(root->left->val != root->right->val)   return false;
-        if(root->left == NULL)  return true;
-        queue<TreeNode *> l;
-        queue<TreeNode *> r;
-        l.push(root->left);
-        r.push(root->right);
-        while(!l.empty())
-        {
-            if(l.front() == NULL && r.front() != NULL){
-                ans = false;
-                break;
-            }
-
-            if(l.front() != NULL && r.front() == NULL){
-                ans = false;
-                break;
-            }
-
-            if(l.front()->val != r.front()->val){
-                ans = false;
-                break;
-            }
-
-            if(l.front()->right != NULL || l.front()->left != NULL){
-                l.push(l.front()->left);
-                l.push(l.front()->right);
-            }
-            if(r.front()->right != NULL || r.front()->left != NULL){
-                r.push(r.front()->right);
-                r.push(r.front()->left);
-            }
-            l.pop();    r.pop();
-        }
-        return ans;
+int workOut(int n,int p){
+    int ans = 0;
+    int op = p;
+    while( n/p>=1 ){
+        ans += n/p;
+        p = p * op;
+    }
+    return ans;
 }
 
 int main(){
-    string strAns="3";
-    while (strAns.size() < 4) {
-        strAns.insert(strAns.begin(), '0');
-    }
-    cout<<strAns<<endl;
+    int n,p;
+    cin >> n >> p;
+    cout<<workOut(n,p);
     return 0;
 }
