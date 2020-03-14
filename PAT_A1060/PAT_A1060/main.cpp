8709054abf63edcfc040a9c4785337b92c59c272
vachel <814018236@qq.com>
2020-03-15 01:54:03 +0800

    总结 A1060
    这算是字符串处理中比较难的一类题了
    对于这种浮点类型数字用stirng类型来存储，有两个关键的点：
    1.首先要排坑，使得多余的零被删掉
    2.要找准小数点的位置，判断数是否大于1，若小于1则小数点出现后，指数位就--
    若大于1则从第一个非零位开始指数位就++

--- PAT_A1060/PAT_A1060/main.cpp
+++ PAT_A1060/PAT_A1060/main.cpp
@@ -9,19 +9,50 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    int n;
-    string a,b;
-    cin>> n >> a >> b;
-    string ansa,ansb;
-    for(int i=0;i<n;i++){
-        ansa[i] = a[i];
-        ansb[i] = b[i];
+int n;
+
+string deal(string s,int & e){
+    int k = 0; //数组下标
+    while(s[0] == '0' && s.length()>0){
+        s.erase(s.begin());
     }
-    if (ansa == ansb)
-        cout<<"YES"<<" ";
-    else
-        cout<<"NO"<<" ";
-    cout<<"0."<<ansa<<"*10^"<<a.size()<<" 0."<<ansb<<"*10^"<<b.size()<<endl;
+    if(s[0] == '.'){
+        s.erase(s.begin()); //去除小数点
+        while(s[0] == '0' && s.length()>0){
+            s.erase(s.begin());
+            e--;
+        }
+    }
+    else{
+        while (s[k] != '.' && k < s.length()) {
+            k++;
+            e++;
+        }
+        if (k < s.length()) {
+            s.erase(s.begin()+k);
+        }
+    }
+    if(s.length() == 0){
+        e = 0;
+    }
+    while (s.length()<n) {
+        s.append("0");
+    }
+    return s.substr(0,n);
+}
+
+int main(int argc, const char * argv[]) {
+    string s1,s2,s3,s4;
+    cin>> n >> s1 >> s2;
+    int e1,e2;
+    e1 = e2 = 0;
+    s3 = deal(s1, e1);
+    s4 = deal(s2, e2);
+    if(s3 == s4 && e1 == e2){
+        cout<<"YES 0."<<s3<<"*10^"<<e1<<endl;
+    }
+    else{
+        cout<<"NO 0."<<s3<<"*10^"<<e1<<" 0."<<s4<<"*10^"<<e2<<endl;;
+    }
     return 0;
 }
