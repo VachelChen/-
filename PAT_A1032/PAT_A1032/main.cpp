0e5f0ff0851f7a8d24172e2fccdcf84159f1d925
vachel <814018236@qq.com>
2020-03-15 20:17:05 +0800

    指针

---
+++ PAT_A1032/PAT_A1032/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PAT_A1032
+//
+//  Created by VachelChen on 2020/3/15.
+//  Copyright © 2020 VachelChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
