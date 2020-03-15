02422daf22abdb41d330e09a5d36e7079516edfb
vachel <814018236@qq.com>
2020-03-15 21:30:50 +0800

    题目总结A1032
    1.以前我是用指针总是在结点里面加入指向结点的指针表示next，若数据不大的话其实可以用int型，这样直接类似于map来表示，不能盲目使用map会超时。
    2.若想保持数的位数来在前面输出0可以使用 %05d

--- PAT_A1032/PAT_A1032/main.cpp
+++ PAT_A1032/PAT_A1032/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
+using namespace std;
+
+const int maxn = 100010;
+
+struct node{
+    char letter;    //该节点内容
+    int next;     //下一个节点
+    bool flag;
+}n[maxn];
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    for(int i = 0;i<maxn;i++){
+        n[i].flag = false;
+    }
+    int start_1,start_2,num;
+    cin >> start_1 >> start_2 >>num;
+    int address,nextAddress;
+    char let;
+    for(int i=0; i<num; i++){
+        cin >> address >> let >> nextAddress;
+        n[address].letter = let;
+        n[address].next = nextAddress;
+    }
+    int p;
+    for(p = start_1; p!=-1; p=n[p].next){
+        n[p].flag = true;
+    }
+    for(p = start_2; p!=-1; p=n[p].next){
+        if(n[p].flag == true){
+            printf("%05d\n",p);
+            return 0;
+        }
+    }
+    cout<<"-1"<<endl;
     return 0;
 }
