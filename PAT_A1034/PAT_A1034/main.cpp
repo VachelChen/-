24fd6589a7f7fee7020f6d81614cfb6bfa23c709
vachel <814018236@qq.com>
2020-03-30 23:56:21 +0800

    x

---
+++ PAT_A1034/PAT_A1034/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  PAT_A1034
+//
+//  Created by VachelChen on 2020/3/30.
+//  Copyright © 2020 VachelChen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
