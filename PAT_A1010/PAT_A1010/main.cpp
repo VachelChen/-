1961793f73c473dbda0567120e9d864099e12708
vachel <814018236@qq.com>
2020-05-15 09:55:09 +0800

    c

---
+++ PAT_A1010/PAT_A1010/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  PAT_A1010
+//
+//  Created by VachelChen on 2020/5/11.
+//  Copyright © 2020 VachelChen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    string n1,n2;
+    cin >> n1 >> n2;
+    int tag,radix;
+    cin >> tag >> radix;
+    
+    return 0;
+}
