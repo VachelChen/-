bb282aae89160a798ac56d07467bac34e508b7ca
vachel <814018236@qq.com>
2020-05-21 00:46:11 +0800

    c

---
+++ PAT_A1015/PAT_A1015/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  PAT_A1015
+//
+//  Created by VachelChen on 2020/5/21.
+//  Copyright © 2020 VachelChen. All rights reserved.
+//
+
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+string i2s(int x){
+    string str;
+    stringstream ss;
+    ss << x;
+    ss >> str;
+    return str;
+}
+
+int s2i(string str){
+    int x;
+    stringstream ss;
+    ss << str;
+    ss >> x;
+    return x;
+}
+
+const int maxn = 10001;
+
+int temp[maxn];
+bool prime[maxn];
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    
+    int n,d;
+    scanf("%d",&n);
+    while (n>=0) {
+        scanf("%d",&d);
+        
+        
+        
+        scanf("%d",&n);
+    }
+    return 0;
+}
