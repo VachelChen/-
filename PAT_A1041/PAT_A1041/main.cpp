360487cdac436fbf2dbfd86fb93f0af7785e3419
vachel <814018236@qq.com>
2020-06-22 19:26:28 +0800

    create

---
+++ PAT_A1041/PAT_A1041/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  PAT_A1041
+//
+//  Created by VachelChen on 2020/6/22.
+//  Copyright © 2020 VachelChen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        
+    }
+    return 0;
+}
